8f8fd79d738c96bbf33582b16deda5a2e2b8ad24
Lautaro <laut.german@gmail.com>
2016-05-19 20:03:55 +0200

    Añade funcion distancia euclidea

---
+++ euclideanDist.m
@@ -0,0 +1,19 @@
+function v = euclideanDist(v1,v2)
+[rows cols] = size(v1);
+aux = 0;
+cont = 1;
+for k = 1:cols
+    for col =  1:cols
+        for row = 1:rows
+        uv2 = v2(row,k)
+        row,k
+        uv1= v1(row,col)
+        row,col
+        aux = aux+(v2(row,k)-v1(row,col))^2;
+        end
+    vaux(cont) = sqrt(aux);
+    cont = cont+1;
+    aux = 0;
+    end
+end
+v = vaux;
