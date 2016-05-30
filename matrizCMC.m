537b7b6ad38e1c1924ad3950efc774b28a39def6
Lautaro Germán <laut.german@gmail.com>
2016-05-30 17:23:17 +0200

    Curva CMC

--- matrizCMC.m
+++ matrizCMC.m
@@ -1,7 +1,8 @@
-function m = matrizCMC(v,n)
+function [r,m]= matrizCMC(v,n,etiquetas)
 cont = 1;
 nCols = n;
 ceros = zeros(nCols);
+ranking = zeros(nCols);
 for i = 1:nCols
     for j = 1:nCols
     x(j,i) = v(cont);
@@ -14,5 +15,7 @@ for i = 1:nCols
     tmp = inds(i,1:length(inds));
     index = find(tmp == i);
     ceros(i,index) = 1;
+    ranking(i,:) = etiquetas(tmp);
 end
 m = ceros;
+r = ranking;
